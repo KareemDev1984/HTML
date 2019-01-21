6349a8611a84bd4d53da24bf28a3a8232ce3a3d4
KARIM LAMRINI <KarimL@cursisten.intecbrussel.intra>
2019-01-21 16:37:43 +0100

    changes

--- dom-examples/src/index.js
+++ dom-examples/src/index.js
@@ -11,48 +11,38 @@ document.body.appendChild(h1El)
 document.getElementById('root').appendChild(woord)
 
 
+
+
+// eerst: yarn add crel,
+// dan:
+const crel = require('crel')
+
 const inbox = [
-    {
-        subject: "Hello man",
-        from: "waldo@wallyworld.com",
-        body: "yo man how youve been doing?"
-    },
-    {
-        subject: "New registration",
-        from: "no-reply@worldonlineforums.com",
-        body: "Welcome to the new WOF..."
-    },
- ]
- 
- function renderMessage(message) {
-    const messageView = document.createElement('li')
- 
-    const subjectView = document.createElement('h1')
-    subjectView.appendChild(document.createTextNode(message.subject))
- 
-    const fromView = document.createElement('a')
-    fromView.appendChild(document.createTextNode(message.from))
- 
-    const bodyView = document.createElement('p')
-    bodyView.appendChild(document.createTextNode(message.body))
- 
-    messageView.appendChild(subjectView)
-    messageView.appendChild(fromView)
-    messageView.appendChild(bodyView)
- 
-    return messageView
- }
- 
- function renderInbox(inbox) {
-    const inboxView = document.createElement('ul')
- 
-    for (let i = 0; i < inbox.length; i++) {
-        const message = inbox[i]
-        inboxView.appendChild(renderMessage(message))
-    }
- 
-    return inboxView
- }
- 
- const view = renderInbox(inbox)
- document.getElementById('root').appendChild(view)
+   {
+       subject: "Hello man",
+       from: "waldo@wallyworld.com",
+       body: "yo man how youve been doing?"
+   },
+   {
+       subject: "New registration",
+       from: "no-reply@worldonlineforums.com",
+       body: "Welcome to the new WOF..."
+   },
+]
+
+function renderMessage(message) {
+   return crel('li',
+       crel('h1', message.subject),
+       crel('a',
+           { href: 'mailto:' + message.from },
+           message.from),
+       crel('p', message.body))
+}
+
+function renderInbox(inbox) {
+   return crel('ul', inbox.map(renderMessage))
+}
+
+const view = renderInbox(inbox)
+document.getElementById('root').appendChild(view)
+
